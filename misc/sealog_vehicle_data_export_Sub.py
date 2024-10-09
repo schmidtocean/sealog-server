f732c75f9ccd39ede995c7c2cfdf4fe0a8574b1f
Kaarel.R <krais@schmidtocean.org>
2024-10-09 15:29:49 +0300

    BUGFIX - EXPORTS - fixed sb_ph_corr prefix

--- misc/sealog_vehicle_data_export_Sub.py
+++ misc/sealog_vehicle_data_export_Sub.py
@@ -106,7 +106,7 @@ DATA_FILES_DEFS = [
     { 'source_regex': '*sb_oxygen-*', 'output_prefix': 'sb_oxygen_'},
     { 'source_regex': '*sb_oxygen_corr-*', 'output_prefix': 'sb_oxygen_corr_'},
     { 'source_regex': '*sb_ph-*', 'output_prefix': 'sb_ph_'},
-    { 'source_regex': '*sb_ph_corr-*', 'output_prefix': 'sb_ph_corr'},
+    { 'source_regex': '*sb_ph_corr-*', 'output_prefix': 'sb_ph_corr_'},
     { 'source_regex': '*sb_paro-*', 'output_prefix': 'sb_paro_'},
     { 'source_regex': '*sb_paro_depth_corr-*', 'output_prefix': 'sb_paro_depth_corr_'},
     { 'source_regex': '*sb_sprint-*', 'output_prefix': 'sb_sprint_'},
