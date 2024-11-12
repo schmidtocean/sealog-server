5624bb46688ddfe131c6251f27028151ebb79db1
Kaarel.R <krais@schmidtocean.org>
2024-11-12 16:49:32 +0200

    EXPORTS - warn with filename

--- misc/filecrop_utility.py
+++ misc/filecrop_utility.py
@@ -156,7 +156,7 @@ class FileCropUtility():
                             line_ts = datetime.strptime(line_str.split(self.delimiter)[0],self.dt_format)
 
                         except Exception as err:
-                            logging.warning("Could not process line: %s", line_str)
+                            logging.warning("Could not process line: %s in %s", line_str, data_file)
                             logging.debug(str(err))
 
                         else:
