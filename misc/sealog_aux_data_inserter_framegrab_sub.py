d836f695299d9c2edef8684ff9f419ba05c3b2a8
Webb Pinner <webbpinner@gmail.com>
2026-08-01 11:25:02 -0400

    refactor Sub framegrab sources to support inline or external YAML config
    Sources now support inline defaults or an external file via -f/--sources_file,
    matching the pattern used by sealog_aux_data_inserter_framegrab_vnc.py and
    sealog_cruise_sync.py. Adds sensible defaults (filename_prefix from
    source_name, filename_suffix to .jpg), an allow-list for filename_suffix,
    and sub_framegrab_sources.yml as a ready-to-use external config example.

--- misc/sealog_aux_data_inserter_framegrab_sub.py
+++ misc/sealog_aux_data_inserter_framegrab_sub.py
@@ -11,13 +11,17 @@ DESCRIPTION:    This service listens for new events submitted to Sealog, copies
                 consider the data stale and will not associate it with the
                 newly created event.
 
+                The sources are configured via the DEFAULT_SOURCES_CONFIG inline
+                YAML block below. Pass --sources_file/-f to load the sources
+                configuration from an external YAML file instead.
+
 BUGS:
 NOTES:
 AUTHOR:     Webb Pinner
 COMPANY:    OceanDataTools.org
-VERSION:    1.0
+VERSION:    1.1
 CREATED:    2020-01-27
-REVISION:   2022-02-28
+REVISION:   2026-08-01
 
 LICENSE INFO:   This code is licensed under MIT license (see LICENSE.txt for details)
                 Copyright (C) OceanDataTools.org 2024
@@ -30,9 +34,10 @@ import json
 import time
 import shutil
 import logging
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from os.path import dirname, realpath
 
+import yaml
 import requests
 import websockets
 
@@ -64,50 +69,42 @@ THRESHOLD = 20  # seconds
 SOURCE_DIR = '/mnt/ramdisk'
 DEST_DIR = '/data/sealog-Sub-files/images'
 
-sources = [
-    {
-        'source_url': 'http://10.23.46.60/images/',
-        'source_filename': 'scicam.jpg',
-        'source_name': 'SCICAM',
-        'filename_prefix': 'SCICAM_',
-        'filename_suffix': '.jpg'
-    },
-    {
-        'source_url': 'http://10.23.46.61/images/',
-        'source_filename': 'sitcam.jpg',
-        'source_name': 'SITCAM',
-        'filename_prefix': 'SITCAM_',
-        'filename_suffix': '.jpg'
-        #    },
-        #    {
-        #        'source_url': 'http://10.23.46.62/images/',
-        #        'source_filename': 'hdquad.jpg',
-        #        'source_name': 'HDQUAD',
-        #        'filename_prefix': 'HDQUAD_',
-        #        'filename_suffix': '.jpg'
-        #    },
-        #    {
-        #        'source_url': 'http://10.23.46.63/images/',
-        #        'source_filename': 'sdquad.jpg',
-        #        'source_name': 'SDQUAD',
-        #        'filename_prefix': 'SDQUAD_',
-        #        'filename_suffix': '.jpg'
-        #    },
-        #    {
-        #        'source_url': 'http://10.23.46.64.images/',
-        #        'source_filename': 'scitoo.jpg',
-        #        'source_name': 'SCITOO',
-        #        'filename_prefix': 'SCITOO_',
-        #        'filename_suffix': '.jpg'
-        #    },
-        #    {
-        #        'source_url': 'http://10.23.46.65/images/',
-        #        'source_filename': 'sittoo.jpg',
-        #        'source_name': 'SITTOO',
-        #        'filename_prefix': 'SITTOO_',
-        #        'filename_suffix': '.jpg'
-    }
-]
+# Allowed image formats for frame grab captures.
+DEFAULT_FILENAME_SUFFIX = '.jpg'
+ALLOWED_FILENAME_SUFFIXES = ('.png', '.jpg', '.bmp')
+
+# Default sources configuration, used unless an external sources file is
+# specified via the --sources_file/-f command-line option.
+# filename_prefix is optional and defaults to source_name when omitted.
+# filename_suffix is optional and defaults to .jpg when omitted; when set
+# it must end with one of ALLOWED_FILENAME_SUFFIXES.
+DEFAULT_SOURCES_CONFIG = '''
+sources:
+  - source_name: SCICAM
+    source_url: http://10.23.46.60/images/
+    source_filename: scicam.jpg
+
+  - source_name: SITCAM
+    source_url: http://10.23.46.61/images/
+    source_filename: sitcam.jpg
+
+  # Additional cameras, uncomment and adjust as needed:
+  # - source_name: HDQUAD
+  #   source_url: http://10.23.46.62/images/
+  #   source_filename: hdquad.jpg
+  #
+  # - source_name: SDQUAD
+  #   source_url: http://10.23.46.63/images/
+  #   source_filename: sdquad.jpg
+  #
+  # - source_name: SCITOO
+  #   source_url: http://10.23.46.64/images/
+  #   source_filename: scitoo.jpg
+  #
+  # - source_name: SITTOO
+  #   source_url: http://10.23.46.65/images/
+  #   source_filename: sittoo.jpg
+'''
 
 HELLO = {
     'type': 'hello',
@@ -125,7 +122,7 @@ PING = {
 }
 
 
-async def aux_data_inserter():
+async def aux_data_inserter(sources):  # pylint:disable=redefined-outer-name
     '''
     Connect to the websocket feed for new events.  When new events arrive,
     build aux_data records and submit them to the sealog-server.
@@ -153,8 +150,8 @@ async def aux_data_inserter():
 
                     if datetime.strptime(
                             event_obj['message']['ts'],
-                            '%Y-%m-%dT%H:%M:%S.%fZ') < datetime.utcnow() - timedelta(
-                            seconds=THRESHOLD):
+                            '%Y-%m-%dT%H:%M:%S.%fZ') < datetime.now(timezone.utc).replace(
+                            tzinfo=None) - timedelta(seconds=THRESHOLD):
                         logging.debug("Skipping because event ts is older than thresold")
                         continue
 
@@ -166,24 +163,16 @@ async def aux_data_inserter():
 
                     for source in sources:
 
-                        filename_date = datetime.date(datetime.strptime(
-                            event_obj['message']['ts'],
-                            '%Y-%m-%dT%H:%M:%S.%fZ')
+                        event_ts = datetime.strptime(
+                            event_obj['message']['ts'], '%Y-%m-%dT%H:%M:%S.%fZ'
                         )
-                        filename_time = datetime.time(
-                            datetime.strptime(
-                                event_obj['message']['ts'],
-                                '%Y-%m-%dT%H:%M:%S.%fZ'
-                            )
-                        )
-                        filename_middle = datetime.combine(
-                            filename_date, filename_time
-                        ).strftime("%Y%m%d_%H%M%S%f")[:-3]
+                        filename_middle = event_ts.strftime("%Y%m%d_%H%M%S%f")[:-3]
 
                         event_slug = event_obj['message']['event_value'].upper().replace(' ', '_')
                         filename = (
-                            f"{source['filename_prefix']}{filename_middle}"
-                            f"_{event_slug}{source['filename_suffix']}"
+                            f"{source.get('filename_prefix', source['source_name'])}"
+                            f"_{filename_middle}_{event_slug}"
+                            f"{source.get('filename_suffix', DEFAULT_FILENAME_SUFFIX)}"
                         )
                         dst = os.path.join(DEST_DIR, filename)
 
@@ -243,6 +232,8 @@ if __name__ == '__main__':
     parser.add_argument('-v', '--verbosity', dest='verbosity',
                         default=0, action='count',
                         help='Increase output verbosity')
+    parser.add_argument('-f', '--sources_file',
+                        help='use the specified sources file instead of the built-in inline config')
 
     parsed_args = parser.parse_args()
 
@@ -257,6 +248,30 @@ if __name__ == '__main__':
     parsed_args.verbosity = min(parsed_args.verbosity, max(LOG_LEVELS))
     logging.getLogger().setLevel(LOG_LEVELS[parsed_args.verbosity])
 
+    # Load the sources config, either from an external file or the
+    # built-in inline default.
+    if parsed_args.sources_file:
+        try:
+            with open(parsed_args.sources_file, 'r', encoding='utf-8') as file:
+                sources_config = yaml.safe_load(file)
+            sources = sources_config['sources']
+        except (OSError, yaml.YAMLError, TypeError, KeyError) as error:
+            logging.error("Could not load sources configuration from file")
+            logging.debug(str(error))
+            sys.exit(1)
+    else:
+        sources_config = yaml.safe_load(DEFAULT_SOURCES_CONFIG)
+        sources = sources_config['sources']
+
+    for src in sources:
+        suffix = src.get('filename_suffix', DEFAULT_FILENAME_SUFFIX)
+        if not suffix.endswith(ALLOWED_FILENAME_SUFFIXES):
+            logging.error(
+                "Invalid filename_suffix '%s' for source '%s': must end with one of %s",
+                suffix, src.get('source_name', '<unknown>'), ALLOWED_FILENAME_SUFFIXES
+            )
+            sys.exit(1)
+
     # Run the main loop
     while True:
 
@@ -265,7 +280,7 @@ if __name__ == '__main__':
 
         try:
             # t.connect(username=user, pkey=my_key) # only needed for scp transfers
-            asyncio.get_event_loop().run_until_complete(aux_data_inserter())
+            asyncio.get_event_loop().run_until_complete(aux_data_inserter(sources))
         except KeyboardInterrupt:
             logging.error('Keyboard Interrupted')
             try:
